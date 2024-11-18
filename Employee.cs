79b1825e65c7d5f70b6fcd419d7887db494e6e35
sajad Huseein <146040134+hhaa3344@users.noreply.github.com>
2024-11-18 15:34:25 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace First_S.Models
+{
+    public class Employee
+    {
+        
+        
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string phone { get; set; }
+            public string email { get; set; }
+        
+    }
+}
---
+++ customer.cs
@@ -0,0 +1,11 @@
+﻿namespace First_S.Models
+{
+    public class customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address {  get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+}
